d19f0490cd2ba2c33089753524e553768a41cf53
Anneke-01 <anneke_paulina11@hotmail.com>
2023-06-13 02:13:35 -0600

    Probando responsive en móvil.

--- js/app2.js
+++ js/app2.js
@@ -1,12 +1,69 @@
+// let menu = document.querySelector('#menu-btn');
+// let navbar = document.querySelector('.header .navbar');
+
+// menu.onclick = () =>{
+//     menu.classList.toggle('fa-times');
+//     navbar.classList.toggle('active');
+
+
+// };
+
+// window.onscroll = () =>{
+//     menu.classList.remove('fa-times');
+//     navbar.classList.remove('active');
+// };
+
+const openModalBtn = document.getElementById("openModal");
+const modal = document.getElementById("modal");
+const closeModalBtn = document.getElementsByClassName("close")[0];
+let isModalActive = false;
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.header .navbar');
 
-menu.onclick = () =>{
+openModalBtn.addEventListener("click", function () {
+    modal.style.display = "block";
+    isModalActive = true;
+    disableMenu();
+});
+
+closeModalBtn.addEventListener("click", function () {
+    modal.style.display = "none";
+    isModalActive = false;
+    enableMenu();
+});
+
+window.addEventListener("click", function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        isModalActive = false;
+        enableMenu();
+    }
+});
+
+
+
+menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
+    disableMenu();
 };
 
-window.onscroll = () =>{
+window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
+    enableMenu();
 };
+
+function disableMenu() {
+    if (isModalActive) {
+        menu.classList.remove('fa-times');
+        navbar.classList.remove('active');
+    }
+}
+
+function enableMenu() {
+    if (!isModalActive) {
+        menu.classList.add('fa-times');
+        navbar.classList.add('active');
+    }
+}
--- js/modal.js
+++ js/modal.js
@@ -1,18 +1,18 @@
-const openModalBtn = document.getElementById("openModal");
-const modal = document.getElementById("modal");
-const closeModalBtn = document.getElementsByClassName("close")[0];
+// const openModalBtn = document.getElementById("openModal");
+// const modal = document.getElementById("modal");
+// const closeModalBtn = document.getElementsByClassName("close")[0];
 
 
-openModalBtn.addEventListener("click", function() {
-  modal.style.display = "block";
-});
+// openModalBtn.addEventListener("click", function() {
+//   modal.style.display = "block";
+// });
 
-closeModalBtn.addEventListener("click", function() {
-  modal.style.display = "none";
-});
+// closeModalBtn.addEventListener("click", function() {
+//   modal.style.display = "none";
+// });
 
-window.addEventListener("click", function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}); 
+// window.addEventListener("click", function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }); 
